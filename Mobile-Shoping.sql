c6a6cec7b64b1fcdbeb1b7f7442e151a7e9e39cf
hienthao339 <hienthao338@gmail.com>
2023-03-25 10:42:39 +0700

    change db

--- Mobile-Shoping.sql
+++ Mobile-Shoping.sql
@@ -2,15 +2,11 @@ create database MobileShopping
 use MobileShopping
 
 --------------------default: insert unrequired--------------------
-create table users
+create table customer 
 (
-	id_user int identity constraint PK_id_user primary key, 
-	is_admin bit default 0,
-	email varchar(100), 
-	passwords varchar(100), 
-	avatar varchar(100),
+	id_customer int constraint PK_id_customer primary key, ----random
 	names varchar(100), 
-	phone varchar(10), 
+	phone varchar(10) unique, 
 	addresss varchar(500), 
 	ranks int	--in (1,2,3,4)
 	---1: Copper
@@ -19,10 +15,19 @@ create table users
 	---4: Diamond
 )
 
+create table users
+(
+	id_user int identity constraint PK_id_user primary key, 
+	is_admin bit default 0,
+	email varchar(100) unique, 
+	passwords varchar(100), 
+	avatar varchar(100)
+)
+
 create table promocode 
 (
 	id_promo int identity constraint PK_id_promo primary key, 
-	code varchar(10),
+	code varchar(10) unique,
 	created_at datetime default getdate(), 
 	started_at datetime default getdate() + 1, 
 	finished_at datetime default getdate() + 15,
@@ -55,21 +60,21 @@ create table product
   battery varchar(100), 
   ram varchar(15), 
   id_brand int constraint FK_id_product_brand foreign key references brand (id_brand),
-  id_color int constraint FK_id_product_brand foreign key references color (id_color),
+  id_color int constraint FK_id_product_color foreign key references color (id_color),
   quantity int default 10,
   rate decimal,
   discount_price decimal
 )
 
 create table cart(
-  id_user_cart int constraint PK_id_user_cart primary key constraint FK_id_user_cart references users(id_user),
+  id__cart int constraint PK_id_cart primary key,
   id_product int constraint FK_id_product_cart foreign key references product (id_product), 
   quantity int 
 )
 
 create table orders(
-  id int identity constraint PK_id_order primary key, 
-  id_user int constraint FK_id_user_order foreign key references users(id_user), 
+  id_order int identity constraint PK_id_order primary key, 
+  id_customer int constraint FK_id_customer_order foreign key references customer(id_customer), 
   payment_type bit default 0, 
   created_at datetime default getdate(), 
   started_at datetime default getdate() + 1, 
@@ -86,7 +91,7 @@ create table orders(
 
 create table order_item(
   id_order_item int identity constraint PK_id_order_item  primary key, 
-  id_order int constraint FK_id_order foreign key references orders(id), 
+  id_order int constraint FK_id_order foreign key references orders(id_order), 
   id_product int constraint FK_id_product_order_item foreign key references product(id_product), 
   quantity int
 )
@@ -124,7 +129,7 @@ INSERT INTO color (names) VALUES
 --++++++++++++++++++++++++++++++++++++++++
 INSERT INTO brand (names) VALUES
 	('Samsung'),
-	('Iphone'),
+	('Apple'),
 	('Oppo'),
 	('Google');
 
@@ -165,10 +170,10 @@ INSERT INTO product (names, images, price, display, weights, water_resistance, o
 	('iPhone 14', 'wwwroot/Images/Products/33.png', 800, '"6.1" Super Retina XDR OLED"', '172 g', 'IP68', 'iOS 16', 'A15 Bionic with 5-core GPU', '3279 mAh,  12.68 Wh', '6 GB', 2, 9),
 	('iPhone 14', 'wwwroot/Images/Products/34.png', 800, '"6.1" Super Retina XDR OLED"', '172 g', 'IP68', 'iOS 16', 'A15 Bionic with 5-core GPU', '3279 mAh,  12.68 Wh', '6 GB', 2, 10),
 	('iPhone 14', 'wwwroot/Images/Products/35.png', 800, '"6.1" Super Retina XDR OLED"', '172 g', 'IP68', 'iOS 16', 'A15 Bionic with 5-core GPU', '3279 mAh,  12.68 Wh', '6 GB', 2, 12),
-	('iPhone 14 Pro ', 'wwwroot/Images/Products/36.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 2),
-	('iPhone 14 Pro', 'wwwroot/Images/Products/37.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 5),
-	('iPhone 14 Pro', 'wwwroot/Images/Products/38.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 9),
-	('iPhone 14 Pro ', 'wwwroot/Images/Products/39.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 11),
+	('iPhone 14 Pro ', 'wwwroot/Images/Products/36.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 2),
+	('iPhone 14 Pro', 'wwwroot/Images/Products/37.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 5),
+	('iPhone 14 Pro', 'wwwroot/Images/Products/38.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 9),
+	('iPhone 14 Pro ', 'wwwroot/Images/Products/39.png', 900, '"6.1" Super Retina XDR OLED"', '206 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '3200 mAh,  12.38 Wh', '6 GB', 2, 11),
 	('iPhone 14 Pro Max', 'wwwroot/Images/Products/40.png', 1200, '"6.7" Super Retina XDR OLED"', '240 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '4323 mAh,  16.68 Wh', '6 GB', 2, 2),
 	('iPhone 14 Pro Max', 'wwwroot/Images/Products/41.png', 1200, '"6.7" Super Retina XDR OLED"', '240 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '4323 mAh,  16.68 Wh', '6 GB', 2, 5),
 	('iPhone 14 Pro Max', 'wwwroot/Images/Products/42.png', 1200, '"6.7" Super Retina XDR OLED"', '240 g', 'IP68', 'iOS 16', 'A16 Bionic chip', '4323 mAh,  16.68 Wh', '6 GB', 2, 9),
@@ -188,66 +193,8 @@ INSERT INTO product (names, images, price, display, weights, water_resistance, o
 	('Pixel 7 Pro', 'wwwroot/Images/Products/56.png', 720, '"6.7" QHD+ LTPO OLED at 512 PPI"', '212 g', 'IP68', '', '4 Tensor G2', '5000 mAh', '12 GB', 4, 2),
 	('Pixel 7 Pro', 'wwwroot/Images/Products/57.png', 720, '"6.7" QHD+ LTPO OLED at 512 PPI"', '212 g', 'IP68', '', '4 Tensor G2', '5000 mAh', '12 GB', 4, 6),
 	('Pixel 7 Pro', 'wwwroot/Images/Products/58.png', 720, '"6.7" QHD+ LTPO OLED at 512 PPI"', '212 g', 'IP68', '', '4 Tensor G2', '5000 mAh', '12 GB', 4, 12);
+
 --++++++++++++++++++++++++++++++++++++++++
-
-INSERT INTO users (is_admin, email , passwords, names , phone , addresss ) VALUES
-	(0, 'ande223@gmail.com', 456, 'Anderson', '6929963841', '21, Kalbadevi Road, Mumbai-2.  '),
-	(0, 'john125@gmail.com', 456, 'John', '1456032778', '49 Princess Street, Mumbai-2.  '),
-	(0, 'beiley267@gmail.com', 456, 'Beiley', '6284691582', '89 Princess Street, R. No. 45, Mumbai-2.  '),
-	(0, 'mark137@gmail.com', 456, 'Mark', '2130077630', 'R.No. 3A,2/22/Babu Genu Road, Princess Street, Mumbai.  '),
-	(0, 'maria246@gmail.com', 456, 'Maria', '6321222854', '11 Magaldas Rd, P.Street, Mumbai-2.  '),
-	(0, ' ', ' ', 'Carma', '6321232854', '78/80 Souri Bldg. B.G. Road, Mumbai.  '),
-	(0, ' ', ' ', 'Cheryl', '2561058706', 'Ldb Mangaldas Bldg. No.1 P. Street, Mumbai-2.  '),
-	(0, ' ', ' ', 'Davis', '2398362872', 'Bhagwati, Kalbadevi Rd, Mumbai.  '),
-	(0, ' ', ' ', 'Jerry', '1421478556', '144, C.P. Tank Road, Mumbai-4.  '),
-	(0, ' ', ' ', 'Edwards', '4421478556', '199 B-Shamaldas Gandhi Marg, Mumbai-2.  '),
-	(0, ' ', ' ', 'Dena', '2999195601', 'Gala No.8 & 9, Amir Ind. Estate, Lower Parel, Mumbai -13.  '),
-	(0, ' ', ' ', 'Golpher', '6886271934', 'Gr. Floor, Villa Jofona Bldg., Sitaladevi Temple Rd., Mahim, Mumbai-16.  '),
-	(0, ' ', ' ', 'James', '7222810176', '5Th Floor, West Block, Room No.1,2,3,4, Mahim, Mumbai-16.  '),
-	(0, ' ', ' ', 'Heinz', '1119831027', '206/2, Citizen Soc., V.S. Marg, Mahim, Mumbai-16.  '),
-	(0, ' ', ' ', 'Sara', '1604415093', '189, Jodia Mansion, G.K. Marg, Lower Parel, Mumbai-13.  '),
-	(0, ' ', ' ', 'Stacey', '1864496526', 'B D Petit Parsee Gen. Hosp., B D Petit Rd., Cumballa Hill, Mumbai-36.  '),
-	(0, ' ', ' ', 'Lucy', '1363238416', 'St. Elizabeth’S Hosp., Walkeshwar, Mumbai- 6  '),
-	(0, ' ', ' ', 'Martinez', '6624409250', 'Shop No.14, H.N. Hospital Bldg., 103, R.M.R. Rd., Girgaon, Mumbai-04.  '),
-	(0, ' ', ' ', 'Frank', '9575271422', 'Shop No.5, Gr. Fl., Old Bhavani House, 94, Eep Gawalia Tank Rd., Mumbai-36.  '),
-	(0, ' ', ' ', 'Robert', '9112869100', 'Shop No.7, Himalaya Chs, R.D. Thandani Marg, Worli Sea Face, Worli, Mumbai-18.  '),
-	(0, ' ', ' ', 'Jennifer', '9802220047', 'Bhatia Hospital, Tardeo, Mumbai-400 007.  '),
-	(0, ' ', ' ', 'Mahesh', '3882223406', 'Dalvi Hosp. Bldg., 38, N.S.Patkar Marg, Gamdevi, Mumbai-400 007.  '),
-	(0, ' ', ' ', 'Alex', '4186059811', '15/17, M.K. Marg, Opp. Charni Rd, Rly. Station, Mumbai -400 004.  '),
-	(0, ' ', ' ', 'Apollo', '5402396312', 'S.S.No.3122 To 3127, Old House No. 163, Ravivar Peth, Phaltan, Dist. Satara.  '),
-	(0, ' ', ' ', 'Sam', '3279631324', 'S.S.No.1138, Upper Ground Floor Gala, Pratapgajgpeth, Radhika Chouk, Satara City, Satara.  '),
-	(0, ' ', ' ', 'Steven', '3147055899', 'S.S.No.805/2, S.No.730/1A, Ground Floor, Patil Hoslpt. A/P Dahivadi, Tal. Man, Dist. Satara.  '),
-	(0, ' ', ' ', 'Tiffani', '8214041514', 'M.No.512, 513, Saraswatichandra Padali  '),
-	(0, ' ', ' ', 'Ariana', '3848461627', 'Station, Satara Rd., Tal. Koregaon, Dist. Satara.  '),
-	(0, ' ', ' ', 'Justin', '2054557060', 'M.No.2/2, Gala No.5, A/P. Vaduj, Tal. Khaur, Dist. Satara.  '),
-	(0, ' ', ' ', 'Hailey', '5233118891', 'M.No.2053, Gala No.1, Neeraj Complex, Gr. Floor, Near Krushna Hospital, A/P. Malkapur, Tal. Karad, Dist. Satara.  '),
-	(0, ' ', ' ', 'Bella', '5330957390', 'M.No.825, Gala No.2, A/P. Maralhaveli, Tal. Patna, Dist. Satara.  '),
-	(0, ' ', ' ', 'Bob', '9169910803', '154/2, Main Road, A/P. Pachgani, Tal. Mahabaleshwar, Dist. Satara.  '),
-	(0, ' ', ' ', 'Karina', '6554637512', '425/A, Gala No.2, Ganpatiali, A/P. Tal. Wai, Dist, Satara.  '),
-	(0, ' ', ' ', 'Dora', '7605125645', 'S.S.No.467/7A/1/42, Room No.1, Gr. Floor, Satara.  '),
-	(0, ' ', ' ', 'Jessie', '6785967790', '22, 23 & 24 Vartak Nagar, Shopkeeper Soc. Vartaknagar Naka, Thane (W).  '),
-	(0, ' ', ' ', 'Kate', '8080670669', 'Shivaji Nagar, Sane Guruji Path, Naupada, Thane (W).  '),
-	(0, ' ', ' ', 'Kris', '9199628314', '5, Ramchandra Chs. Lokmanya Nagar, Pada  '),
-	(0, ' ', ' ', 'Layla', '1237318462', 'No.2, Near Devendra Indl. Estate, Thane (W).  '),
-	(0, ' ', ' ', 'Lili', '1828394198', '15, Ameet Anand Society, Pipe Line Road, Panch, Pakhadi, Thane (W).  '),
-	(0, ' ', ' ', 'Mary', '1601971362', '2, Sainath View, Behind Anand Cinema, Kopri Colony, Thane (W).  '),
-	(0, ' ', ' ', 'Rosia', '7691731675', 'Shop No.687, St. Baptist School Compound, Old Mumbai Road, Opp. Brahman Vidalaya, Thane (W).  '),
-	(0, ' ', ' ', 'Sophia', '6850309362', 'Chatrapati Shivaji Maharaj Hospital, T.M.C. Kalwa, Dist. Thane.  '),
-	(0, ' ', ' ', 'Veronica', '7835095243', '2, Prathamesh Apt., Gen, A.K. Vaidya North, Panchpakhadi, Thane.  '),
-	(0, ' ', ' ', 'Sunny', '7983499576', 'R. No.1, Gr. Flr, Voltas Comp., Enclave,  '),
-	(0, ' ', ' ', 'Stella', '3580943062', 'R. No.1, Basement, Voltas Comp., Enclave, Express Highway, Service Road, Thane (W).  '),
-	(0, ' ', ' ', 'Miley', '7108216178', 'No.25, Amrapali Arcade, Pokharan Road No.2, Vasant Vihar, Thane (W).  '),
-	(0, ' ', ' ', 'Hank', '8046047798', 'House No.581B, Shop No.1, Khanna Apt., Kumbhar Wada, Tal. Uran.  '),
-	(0, ' ', ' ', 'Henry', '7108216178', 'Gala No.3, Mid Town Herritage, Piramal Developer, Dist. Khopoli, Tal. Khalapur.  '),
-	(0, ' ', ' ', 'Jane', '8046047798', 'Amira Chs, Gr. Flr, Gala No.5, At Post Tal. Karjat, Dist. Raigad.  '),
-	(0, ' ', ' ', 'Scott', '7023664117', 'House No.1902, Ward No.17, Tal. Shrivardhan, Dist. Raigad.  '),
-	(0, ' ', ' ', 'Ben', '9236217384', 'House No.54, Ward No.17, Post Murud-Janjira, Tal. Murud, Dist. Raigad.  '),
-	(1, 'nghiadang@gmail.com', 123, ' ', ' ', ' '),
-	(1, 'ptchau@gmail.com', 123, ' ', ' ', ' '),
-	(1, 'ththao@gmail.com', 123, ' ', ' ', ' '),
-	(1, 'nqnhat@gmail.com', 123, ' ', ' ', ' ')
-
---+++++++++++++++++++++++++++++++++++++++++++++++++++++
 INSERT INTO promocode (code, discount_price ) VALUES
 	('SAVE15', 15),
 	('SAVE16', 16),
@@ -256,17 +203,8 @@ INSERT INTO promocode (code, discount_price ) VALUES
 	('SAVE19', 19),
 	('SAVE20', 20)
 
---+++++++++++++++++++++++++++++++++++++++++++++++++++++
-INSERT INTO orders (id_user, payment_type, created_at, started_at, finished_at , total_price , pending , delivering , successed , canceled , paid ) VALUES
-	(3, 0, '8/3/2023', '9/3/2023', '10/3/2023', '2298', 1, 1, 1, 0, 1);
 
---+++++++++++++++++++++++++++++++++++++++++++++++++++++
-INSERT INTO order_item(id_order , id_product , quantity) VALUES
-	(1, 1, '1 ');
-
---+++++++++++++++++++++++++++++++++++++++++++++++++++++
-INSERT INTO cart (id_user_cart, id_product, quantity) VALUES ()
-	--51 user
-
+INSERT INTO users (is_admin, email, passwords) VALUES
+	(1, 'thao@gmail.com', 123)
 
 
