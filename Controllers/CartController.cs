9e8804d301472d741f86203094136df7401ea5db
nhatnguyen130102 <nhatnguyenn1412@gmail.com>
2023-03-31 09:39:13 +0700

    add UserController.cs and CartController.cs

---
+++ Controllers/CartController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO.Ports;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebApplication1.Models;
+using WebApplication1.Models.Functions;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        MobileShoppingEntities db = new MobileShoppingEntities();
+        //Add_Items
+        public ActionResult Add_Items(int id, int add_quantity = 1)
+        {
+            if (Session["email"] != null)
+            {
+                user cus = Session["email"] as user;
+                var carts = db.carts.Where(x => x.id_user == cus.id_user).ToList();
+                var pro = db.products.Where(x => x.id_product == id).First();
+                if (pro != null || pro.quantity >= add_quantity)
+                {
+                    var cart = db.carts.Where(x => x.id_product == id && x.id_user == cus.id_user).FirstOrDefault();
+                    if (cart != null)
+                    {
+                        cart cart_user = new cart()
+                        {
+                            id_product = id,
+                            id_user = cus.id_user,
+                            quantity = add_quantity + 1,
+                        };
+                        var fcart = new Func_Cart();
+                        fcart.Update(cart_user);
+                    }
+                    if (cart == null)
+                    {
+
+                        cart cart_user = new cart()
+                        {
+                            id_product = id,
+                            id_user = cus.id_user,
+                            quantity = add_quantity,
+                        };
+                        var fcart = new Func_Cart();
+                        fcart.Insert(cart_user);
+                    }
+                }
+            }
+            else
+            {
+                if (Session["gust"] == null)
+                {
+                    user user = new user()
+                    {
+                        //user_type = "gust"
+                    };
+                    db.users.Add(user);
+                    db.SaveChanges();
+                    Session["gust"] = user;
+                    var cart_user = db.carts.Where(x => x.id_user == user.id_user).ToList();
+                }
+                user cus = Session["gust"] as user;
+                var carts = db.carts.Where(x => x.id_user == cus.id_user).ToList();
+
+                var pro = db.products.Where(x => x.id_product == id).First();
+                if (pro != null || pro.quantity >= add_quantity)
+                {
+                    var cart = db.carts.Where(x => x.id_product == id && x.id_user == cus.id_user).FirstOrDefault();
+                    if (cart != null)
+                    {
+                        cart cart_gust = new cart()
+                        {
+                            id_product = id,
+                            id_user = cus.id_user,
+                            quantity = add_quantity + 1,
+                        };
+                        var fcart = new Func_Cart();
+                        fcart.Update(cart_gust);
+                    }
+                    if (cart == null)
+                    {
+                        cart cart_gust = new cart()
+                        {
+                            id_product = id,
+                            id_user = cus.id_user,
+                            quantity = add_quantity,
+                        };
+                        var fcart = new Func_Cart();
+                        fcart.Insert(cart_gust);
+                    }
+                }
+            }
+            return RedirectToAction("Cart_User", "Cart");
+        }
+        //Cart_User
+        public ActionResult Cart_User()
+        {
+            user cus = Session["email"] as user;
+            if (cus != null)
+            {
+                var cart_user = db.carts.Where(x => x.id_user == cus.id_user).ToList();
+                return View(cart_user);
+            }
+            else
+            {
+                user user = new user()  
+                {
+                    //user_type = "gust"
+                };
+                db.users.Add(user);
+                db.SaveChanges();
+                Session["gust"] = user;
+                var cart_user = db.carts.Where(x => x.id_user == user.id_user).ToList();
+                return View(cart_user);
+            }
+        }
+        //Update_Quantity
+        public ActionResult UpdateQuanity(FormCollection form)
+        {
+            int id = int.Parse(form["ID_PRO_CART"]);
+            int new_quantity = int.Parse(form["NEW_QUANTITY"]);
+            int id_user = int.Parse(form["ID_USER"]);
+            cart item = db.carts.Where(x => x.id_product == id && x.id_user == id_user).FirstOrDefault();
+            if( item != null && item.quantity >= new_quantity)
+            {
+                cart carts = db.carts.Find(item.id_cart);
+                carts.id_product = item.id_product;
+                carts.id_user = item.id_user;
+                carts.quantity = item.quantity;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Cart_User", "Cart");
+        }
+        //Cart/Remove_Item
+        public ActionResult Remove_Item(int id)
+        {
+            user cus = Session["email"] as user;
+            cart item = db.carts.Where(x => x.id_product == id && x.id_user == cus.id_user).FirstOrDefault();
+            db.carts.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Cart_User", "Cart");
+        }
+        //Quantity_Cart
+        public PartialViewResult Quantity_Cart()
+        {
+            int quantity_cart = 0;
+            List<cart> carts = new List<cart>().Where(x => x.id_user == (int)Session["ID_CUS"]).ToList();
+            if (carts != null)
+            {
+                quantity_cart = (int)carts.Sum(x => x.id_product);
+            }
+            ViewBag.quantity_cart = quantity_cart;
+            return PartialView("Quantity_Cart");
+        }
+        //Order_Success
+        public ActionResult Order_Success()
+        {
+            return View();
+        }
+        //Order_CheckOut
+        public ActionResult Order_CheckOut(FormCollection form)
+        {
+            try
+            {
+                var code = form["PROMO_CODE"];
+                var promo = db.promocodes.Where(x => x.code == code).FirstOrDefault();
+                List<cart> carts = new List<cart>().Where(x => x.id_user == (int)Session["ID_CUS"]).ToList();
+                order orders = new order()
+                {
+                    id_user = (int)Session["ID_CUS"],
+                    payment_type = true,
+                    finished_at = null,
+                    shipping_fee = 10,
+                    total_price = 10,
+                    id_promo = promo.id_promo,
+                    pending = true,
+                    delivering = false,
+                    successed = false,
+                    canceled = false,
+                    paid = false,
+                    created_at = DateTime.Now,
+                    city = form["CITY"],
+                    district = form["DISTRICT"],
+                    ward = form["WARD"],
+                    addresss = form["ADDRESS"],
+                };
+                var forder = new Func_Order();
+                forder.Insert(orders);
+                carts.Clear();
+                return RedirectToAction("Order_Success", "Cart");
+            }
+            catch
+            {
+                return Content("Error Check Out. Please infomation of Customer...");
+            }
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+using WebApplication1.Models.Functions;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        MobileShoppingEntities db = new MobileShoppingEntities();
+        [AllowAnonymous]
+        public ActionResult SignIn()
+        {
+            if (Session["email"] != null) return RedirectToAction("index", "Home");
+            return View(new SignInVM());
+        }
+        [AllowAnonymous]
+        public ActionResult SignUp()
+        {
+            if (Session["email"] != null) return RedirectToAction("SignUp", "User");
+            return View(new SignUpVM());
+        }
+        public ActionResult Account()
+        {
+            if (Session["email"] == null)
+            {
+                Session["TrangTruoc"] = Request.RawUrl;
+                return Redirect("SignIn");
+            }
+            var TaiKhoan = (user)Session["email"];
+            var TaiKhoanAccount = new user
+            {
+                email = TaiKhoan.email,
+                passwords = TaiKhoan.passwords,
+            };
+            return View(TaiKhoanAccount);
+        }
+     
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(SignInVM tk)
+        {
+            if (ModelState.IsValid)
+            {
+                var list = db.users.Where(m => m.email == tk.email && m.passwords == tk.password).ToList();
+                if (list.Count == 0)
+                {
+                    ModelState.AddModelError("email", "Tài Khoản hoặc Mật Khẩu không chính xác");
+                    return View(tk);
+                }
+                user taiKhoan = list.First();
+                if (taiKhoan.is_admin != false)
+                {
+                    FormsAuthentication.SetAuthCookie(taiKhoan.email, tk.AutoLogin);
+                    return RedirectToAction("Index", "Admin");
+                }
+                Session["email"] = taiKhoan;
+                if (tk.AutoLogin)
+                {
+                    HttpCookie ckTaiKhoan = new HttpCookie("email"), ckpasswords = new HttpCookie("passwords");
+                    ckTaiKhoan.Value = taiKhoan.email; ckpasswords.Value = taiKhoan.passwords;
+                    ckTaiKhoan.Expires = DateTime.Now.AddDays(15);
+                    ckpasswords.Expires = DateTime.Now.AddDays(15);
+                    Response.Cookies.Add(ckTaiKhoan);
+                    Response.Cookies.Add(ckpasswords);
+                }
+                if (Session["TrangTruoc"] != null)
+                {
+                    return Redirect(Session["TrangTruoc"].ToString());
+                }
+                return RedirectToAction("index", "Home");
+            }
+            return View(tk);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(SignUpVM tk)
+        {
+            if (ModelState.IsValid)
+            {
+                var taiKhoan = db.users.Where(x => x.email == tk.Email).FirstOrDefault();
+                if (taiKhoan != null)
+                {
+                    ModelState.AddModelError("Email", "Tài Khoản đã tồn tại");
+                    return View(tk);
+                }
+                taiKhoan = new user()
+                {
+                    id_user = tk.id_user,
+                    email = tk.Email,
+                    passwords = tk.Password,
+                    is_admin = false,
+                    //user_type = "user",
+                };
+                var hTK = new Func_User();
+                hTK.Insert(taiKhoan);
+                return RedirectToAction("SignIn", "User");
+            }
+            return View(tk);
+        }
+        public ActionResult LogOut()
+        {
+            Session["email"] = null;
+            HttpCookie ckTaiKhoan = new HttpCookie("email"), ckpasswords = new HttpCookie("passwords");
+            ckTaiKhoan.Expires = DateTime.Now.AddDays(-1);
+            ckpasswords.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(ckTaiKhoan);
+            Response.Cookies.Add(ckpasswords);
+            return RedirectToAction("SignIn", "User");
+        }
+        public ActionResult Edit_Info(int id)
+        {
+            var user = db.users.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit_Info(user model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["email"] = model;
+                var fuser = new Func_User();
+                fuser.Update(model);
+                return RedirectToAction("Details_Info", "User", new { id = model.id_user });
+            }
+            return View(model);
+        }
+        public ActionResult Details_Info(int id)
+        {
+            user user = db.users.Find(id);
+            return View(user);
+        }
+    }
+}
