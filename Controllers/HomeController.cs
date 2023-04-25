6f5b93b3eb4d7b320d3bc6d8f25d019a7af3b22f
hienthao339 <hienthao338@gmail.com>
2023-04-25 20:45:15 +0700

    func Feedbacks for each Product

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -133,6 +133,28 @@ namespace WebApplication1.Controllers
             return View(pro_seri);
         }
 
+        public ActionResult Feedbacks(string comment, int rating, int id_pro)
+        {
+            var content = comment;
+            feedback new_fb = new feedback();
+            new_fb.content = content;
+            new_fb.created_at = DateTime.Now;
+            int id = id_pro;
+            if (Session["email"] == null)
+            {
+                this.AddNotification("You must be logged in to rate", NotificationType.ERROR);
+                return RedirectToAction("Details_Pro", "Home", new { id = id_pro });
+            }
+            user user = Session["email"] as user;
+            new_fb.id_user = user.id_user;
+            new_fb.id_product = id;
+            new_fb.rate = rating;
+            db.feedbacks.Add(new_fb);
+            db.SaveChanges();
+            return RedirectToAction("Details_Pro", "Home", new { id = id_pro });
+        }
+
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
